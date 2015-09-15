65f7359dc02f50ba8361f7a69b2ef03f0676d92e
derekdong7913 <derekdong7913@digiwin.biz>
2015-09-15 11:54:45 +0800

    first commit

---
+++ src/ios/testPlugin.h
@@ -0,0 +1,13 @@
+//
+//  testPlugin.h
+//  HelloWorld
+//
+//  Created by Dong on 2015/9/15.
+//
+//
+
+#import <Cordova/Cordova.h>
+
+@interface testPlugin : CDVPlugin
+
+@end
---
+++ src/ios/testPlugin.m
@@ -0,0 +1,13 @@
+//
+//  testPlugin.m
+//  HelloWorld
+//
+//  Created by Dong on 2015/9/15.
+//
+//
+
+#import "testPlugin.h"
+
+@implementation testPlugin
+
+@end
